10bee2da7b9dc2ddf141bc4337f8fbd14f530de8
vsouhrada <vsouhrada@1dbfee8b-0831-45e5-8e27-c0cd3e7bd956>
2009-08-14 08:51:04 +0000

    git-svn-id: https://svn.origo.ethz.ch/juigle/trunk/Juigle@8 1dbfee8b-0831-45e5-8e27-c0cd3e7bd956

--- src/nezarazeno/ILanguage.java
+++ src/nezarazeno/ILanguage.java
@@ -1,7 +1,22 @@
 package nezarazeno;
 
+import ch.ethz.origo.juigle.application.exceptions.JUIGLELangException;
+
+/**
+ * 
+ * 
+ * @author Vaclav Souhrada (v.souhrada@gmail.com)
+ * @version 0.1.0 08/11/09
+ * @since 0.1.0
+ */
 public interface ILanguage {
 	
-	public void updateText();
+	public void setLocalizedResource();
+	
+	public void setResourceBundlePath(String path);
+	
+	public void setResourceBundleKey(String key);
+	
+	public void updateText() throws JUIGLELangException;
 
 }
