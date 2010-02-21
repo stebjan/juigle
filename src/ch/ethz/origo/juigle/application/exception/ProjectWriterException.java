4f682372657603d28c7e3a6e17d01999a0b2e93e
vsouhrada <vsouhrada@1dbfee8b-0831-45e5-8e27-c0cd3e7bd956>
2010-02-21 14:48:23 +0000

    git-svn-id: https://svn.origo.ethz.ch/juigle/trunk/Juigle@54 1dbfee8b-0831-45e5-8e27-c0cd3e7bd956

---
+++ src/ch/ethz/origo/juigle/application/exception/ProjectWriterException.java
@@ -0,0 +1,67 @@
+package ch.ethz.origo.juigle.application.exception;
+
+/**
+ * 
+ * 
+ * @author Vaclav Souhrada (v.souhrada at gmail.com)
+ * @version 0.1.0 (2/21/2010)
+ * @since 0.1.0 (05/18/09)
+ * @see Exception
+ * 
+ */
+public class ProjectWriterException extends Exception {
+
+	/**
+	 * Only for serialization
+	 */
+	private static final long serialVersionUID = 4495599664837030072L;
+	
+	/**
+   * Constructs a new ProjectWriterException exception with the specified cause and a detail
+   * message of <tt>(cause==null ? null : cause.toString())</tt> (which
+   * typically contains the class and detail message of <tt>cause</tt>).
+   * This constructor is useful for exceptions that are little more than
+   * wrappers for other throwables (for example, {@link
+   * java.security.PrivilegedActionException}).
+   *
+   * @param  cause the cause (which is saved for later retrieval by the
+   *         {@link #getCause()} method).  (A <tt>null</tt> value is
+   *         permitted, and indicates that the cause is nonexistent or
+   *         unknown.)
+   */
+  public ProjectWriterException(Throwable cause) {
+      super(cause);
+  }
+  
+  /**
+   * Constructs a new exception with the specified detail message.  The
+   * cause is not initialized, and may subsequently be initialized by
+   * a call to {@link #initCause}.
+   *
+   * @param   message   the detail message. The detail message is saved for 
+   *          later retrieval by the {@link #getMessage()} method.
+   */
+  public ProjectWriterException(String message) {
+  	super(message);
+  }
+
+	/**
+	 * Constructs a new ProjectWriterException exception with the specified detail message and cause.
+	 * <p>
+	 * Note that the detail message associated with <code>cause</code> is <i>not</i>
+	 * automatically incorporated in this exception's detail message.
+	 * 
+	 * @param message
+	 *          the detail message (which is saved for later retrieval by the
+	 *          {@link #getMessage()} method).
+	 * @param cause
+	 *          the cause (which is saved for later retrieval by the
+	 *          {@link #getCause()} method). (A <tt>null</tt> value is
+	 *          permitted, and indicates that the cause is nonexistent or
+	 *          unknown.)
+	 */
+	public ProjectWriterException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
