63c090d95bbfa659e4a74a42b9b6a5220777d8a9
vsouhrada <vsouhrada@1dbfee8b-0831-45e5-8e27-c0cd3e7bd956>
2010-03-14 14:11:48 +0000

    Updated observable - added a new method notifyObservers(Object obj)
    git-svn-id: https://svn.origo.ethz.ch/juigle/trunk/Juigle@60 1dbfee8b-0831-45e5-8e27-c0cd3e7bd956

--- src/ch/ethz/origo/juigle/data/tables/Record.java
+++ src/ch/ethz/origo/juigle/data/tables/Record.java
@@ -1,22 +1,24 @@
 package ch.ethz.origo.juigle.data.tables;
 
 /**
+ * Abstract class for classes (objects) which represents any type of record and
+ * need use ID. For example it should be used for database records.
  * 
  * @author Vaclav Souhrada
- * @version 0.1.0 (11/25/09)
- * @since 0.1.0
- *
+ * @version 0.1.1 (03/10/2010)
+ * @since 0.1.0 (11/25/09)
+ * 
  */
 public abstract class Record {
-	
+
 	private Object id;
 
 	public void setId(Object id) {
 		this.id = id;
 	}
-	
+
 	public Object getId() {
 		return id;
 	}
-	
+
 }
