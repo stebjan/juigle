5bf68ad53de0342db882d33f44c02f3f7807a9ea
vsouhrada <vsouhrada@1dbfee8b-0831-45e5-8e27-c0cd3e7bd956>
2010-03-24 07:35:43 +0000

    Added JUIGLEObservable to JUIGLEFrame for action with frame (close, maximalize, minimalize). Now is question what will be with package listener...
    git-svn-id: https://svn.origo.ethz.ch/juigle/trunk/Juigle@69 1dbfee8b-0831-45e5-8e27-c0cd3e7bd956

--- src/ch/ethz/origo/juigle/application/listener/AppButtonsEvent.java
+++ src/ch/ethz/origo/juigle/application/listener/AppButtonsEvent.java
@@ -2,6 +2,13 @@ package ch.ethz.origo.juigle.application.listener;
 
 import java.util.EventObject;
 
+/**
+ * 
+ * @author Vaclav Souhrada (v.souhrada at gmail.com)
+ * @version 0.1.0 08/12/09
+ * @since 0.1.0 (05/18/09)
+ *
+ */
 public class AppButtonsEvent extends EventObject {
 
   /**
--- src/ch/ethz/origo/juigle/application/listener/AppButtonsListener.java
+++ src/ch/ethz/origo/juigle/application/listener/AppButtonsListener.java
@@ -25,7 +25,13 @@ package ch.ethz.origo.juigle.application.listener;
 import java.util.EventListener;
 
 
-
+/**
+ * 
+ * @author Vaclav Souhrada (v.souhrada at gmail.com)
+ * @version 0.1.0 08/12/09
+ * @since 0.1.0 (05/18/09)
+ *
+ */
 public interface AppButtonsListener extends EventListener {
 	
 	/**
