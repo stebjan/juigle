5da202e3c42bd3c12a942484144e20326c810ed6
vsouhrada <vsouhrada@1dbfee8b-0831-45e5-8e27-c0cd3e7bd956>
2010-11-14 06:41:48 +0000

    * Class Database which represents database connected object. Allow to you get database connection a connect to database.
    * Added a DatabaseDialog class for connect to database. It is not complete yet.
    
    git-svn-id: https://svn.origo.ethz.ch/juigle/branches/1.0.x@145 1dbfee8b-0831-45e5-8e27-c0cd3e7bd956

---
+++ src/ch/ethz/origo/juigle/application/db/Database.java
@@ -0,0 +1,60 @@
+package ch.ethz.origo.juigle.application.db;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.jdesktop.swingx.auth.JDBCLoginService;
+
+import ch.ethz.origo.juigle.application.exception.database.DatabaseException;
+
+/**
+ * 
+ * @author Vaclav Souhrada
+ * @version 0.1.0.00
+ * @since 1.0.x
+ *
+ */
+public class Database {
+
+	private static Connection conn;
+
+	private static JDBCLoginService service;
+
+	public Database() {
+	}
+
+	public void connect(String jdbcClass, String address) {
+		service = new JDBCLoginService(jdbcClass, address);
+	}
+
+	/**
+	 * Close connection with database
+	 * 
+	 * @throws DatabaseException
+	 */
+	public static synchronized void closeConnection() throws DatabaseException {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				throw new DatabaseException(e);
+			}
+		}
+	}
+
+	public JDBCLoginService getLoginServiceInstance() {
+		return service;
+	}
+
+	/**
+	 * Return database connection
+	 * 
+	 * @return actual database connection
+	 */
+	public static Connection getConnection() {
+		if (conn == null) {
+			conn = service.getConnection();
+		}
+		return conn;
+	}
+}
